7358454ed4c5f129d99fc68c6dc79dc779c964e6
Kanteev Leonid <lkolt2@mail.ru>
2015-09-27 19:25:31 +0300

    BitAnd

---
+++ BitAnd/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int BitAnd(int x, int y) // res = ~((~x) | (~y));
+{
+    int ax = ~x;
+    int ay = ~y;
+    int ares = ax | ay;
+    int res = ~ares;
+    return res;
+}
+
+int main()
+{
+
+    int x, y;
+    scanf("%i %i", &x, &y);
+    printf("%i", BitAnd(x, y));
+
+    return 0;
+}
