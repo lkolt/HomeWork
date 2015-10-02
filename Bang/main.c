8d0745e6251bbcaedd71489510d5ee540d50480b
Kanteev Leonid <lkolt2@mail.ru>
2015-10-02 11:43:49 +0300

    FixBang

--- Bang/main.c
+++ Bang/main.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int bang(int x)
+int Bang(int x)
 {
-    int res = ((~((x << 31) >> 31)) + 2) & 1;
+    int s = (x >> 31);                       // return -1: x < 0; 0: x >= 0;
+    int mabsx = ((x & s) | ((~x + 1) & ~s)); // return -x: x >= 0; x: x < 0;
+    int res = ((~(mabsx >> 31)) + 2) & 1;
     return res;
 }
 
@@ -11,6 +13,7 @@ int main()
 {
     int x;
     scanf("%d", &x);
-    printf("%d", bang(x));
+    printf("%d", Bang(x));
+
     return 0;
 }
